fe5b617ab7e9f893c89930c285e385d4765d6dd6
unknown <kovgankosvetlana.com>
2021-10-27 17:31:29 +0300

    Задача с собакой

---
+++ DOG/Program.cs
@@ -0,0 +1,25 @@
+﻿int VFriend1 = 1;
+int VFriend2 = 2;
+int VDog = 5;
+double distance = 10000;
+int count = 0;
+int Friend = 2;
+double time = 0;
+
+while (distance<10)
+
+{
+    if (Friend==1)
+    {
+         time= distance/(VFriend1+VDog);
+         Friend = 2;
+    }
+    else 
+    {
+         time= distance/(VFriend2+VDog);
+         Friend = 1;
+    }
+ distance=distance-(VFriend1+VFriend2)*time;
+ count++;
+}
+Console.WriteLine(count);
