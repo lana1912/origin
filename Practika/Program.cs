6475f3fd8b8a7f3ecf3cdb3a9b58b038234810bc
unknown <kovgankosvetlana.com>
2021-10-27 14:50:08 +0300

    Zadachi

---
+++ Practika/Program.cs
@@ -0,0 +1,65 @@
+﻿//*Задача на перестановку переменных*
+//int a = 3;
+//int b = 1;
+//int temp; 
+//Console.WriteLine($"a= {a} b= {b}");
+//Console.WriteLine("a = " + a + " b = " + b);
+
+//Console.Write(" a= ");
+//Console.Write(a);
+//Console.Write(" b= ");
+//Console.WriteLine(b);
+
+//temp = a;
+//a = b;
+//b = temp;
+//Console.WriteLine($"a= {a} b= {b}");
+
+//Console.WriteLine("a = " + a + " b= " + b);
+
+//Console.Write(" a= ");
+//Console.Write(a);
+//Console.Write(" b= ");
+//Console.Write(b);
+
+//*Задача на выбор наибольшего числа из рандома*
+//int a= new Random().Next(1, 100);
+//int b= new Random().Next(1, 100);
+
+//Console.WriteLine($"a= {a} b= {b}");
+
+//if (a>b)
+//{
+  //Console.WriteLine(a);
+//}
+//else
+//{
+  //  Console.WriteLine(b);
+//}
+
+//*Задача с циклом на квадрат*
+//int current = 1;
+
+//while (current <= 10)
+ //{
+   //  Console.WriteLine($"{current} {current*current}");
+    // current++;
+
+ //}
+
+//*Задача на нахождение числа делящегося на 3 и 5*
+//int start = 2;
+//int end = 128;
+//int current = start;
+ //while (current <= end)
+ //{
+   //  if ((current % 3 == 0) && (current % 5 == 0))
+    //{
+         
+        //  Console.WriteLine(current);
+    //current++;
+//}
+ 
+//   current++;
+ //}
+ 
