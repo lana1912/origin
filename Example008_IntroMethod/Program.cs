976f1ebdd1111d78bfd2454aee564cfb5f79a9c2
unknown <kovgankosvetlana.com>
2021-11-02 13:57:30 +0300

    Начало функций

---
+++ Example008_IntroMethod/Program.cs
@@ -0,0 +1,10 @@
+﻿int max (int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2> result) result = arg2;
+    if (arg3>result) result = arg3;
+    return result;
+
+}
+
+int max1 = max(a1, b1, c1);
